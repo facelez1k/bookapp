7757652d36841af55e3bc7670eb37995aaa8ff45
facelez1k <facelez1k@mail.ru>
2024-10-04 13:54:22 +0300

    second commit

--- rollup.config.js
+++ rollup.config.js
@@ -7,5 +7,7 @@ export default {
         dir: "dist",
         format: "iife"
     },
-    plugins: [css(), nodeResolve()]
+    plugins: [nodeResolve(),css({
+            output: "bundle.css"
+        }) ]
 }
--- src/app.js
+++ src/app.js
@@ -1,22 +1,28 @@
+import { FavoritesView } from "./views/favorites/favorites";
 import { MainView } from "./views/main/main"
 
 class App {
     routes = [
-        {path: "", view: MainView}
+        {path: "", view: MainView},
+        {path: "#favorites", view: FavoritesView},
     ]
+
+    appState = {
+        favorites: []
+    }
+
     constructor() {
-        window.addEventListener('hashchange', this.route.bind(this))
-        this.route()
+        window.addEventListener('hashchange', this.route.bind(this));
+        this.route();
     }
 
     route() {
         if (this.currentView) {
-        this.currentView.destroy()
+        this.currentView.destroy();
         }
-        const view = this.routes.find(r => r.path == location.hash).view
-        this.currentView = new view()
-        this.currentView.render()
-        
+        const view = this.routes.find(r => r.path == location.hash).view;
+        this.currentView = new view(this.appState);
+        this.currentView.render();
     }
 
 }
